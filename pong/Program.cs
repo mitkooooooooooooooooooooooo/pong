4613b842f764bcd5977dcd8ecfafee0ae149e470
mitkooooooooooooooooooooooo <146818015+mitkooooooooooooooooooooooo@users.noreply.github.com>
2024-05-29 14:28:03 +0300

    pon pong dont work

---
+++ pong/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace pong
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Field field = new(51, 15);
+            Console.SetWindowSize(field.GetCols + 1, field.GetRows + 5);
+
+            Console.CursorVisible = false;
+        }
+
+        static void DrawAt(int x, int y, string s)
+        {
+            Console.SetCursorPosition(y, x);
+            Console.WriteLine(s);
+
+            while (true)
+            { 
+              for(int i = 0; i < Field.GetRows; i++)
+              {
+                    for (int j = 0; j < Field.GetCols; j++)
+                    {
+                        DrawAt(i, j, Field.Get(i, j).ToString());
+                    }
+              }
+                Thread.Sleep(10);
+            }
+
+        }
+
+
+
+
+    }
+
+}
---
+++ pong/pongclass.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace pong 
+{
+    public class Field
+    {
+        private readonly char[,] _field;
+
+        public Field(int width, int height, char tile = '#')
+        {
+            this._field = new char[height, width];
+
+            for (int i = 0; i < height; i++)
+            {
+                this.Set(0, i, tile);
+                this.Set(this.GetRows - 1, i, tile);
+            }
+
+                this.Tile = tile;
+
+            
+
+        }
+        public int GetRows => this._field.GetLength(0);
+        public int GetCols => this._field.GetLength(1);
+
+        public char Tile { get; }
+
+        public char Get(int row, int col)
+        {
+            return this._field[row, col];
+        }
+        public void Set(int row, int col, char el)
+        {
+            this._field[row, col] = el;
+        }
+
+
+
+
+
+    }
+
+    //public Field(int width, int height, char tile = '#')
+    //    {
+    //     
+    //    }
+
+            
+
+}
+
+
