dc388254a469dea69f696fa1692870133c3d8656
mitkooooooooooooooooooooooo <146818015+mitkooooooooooooooooooooooo@users.noreply.github.com>
2024-06-01 14:15:55 +0300

    Somthing not supposed to be here but i c am=nt bother with github

---
+++ OddNums/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System.ComponentModel;
+
+namespace OddNums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+        static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            var reader = new StreamReader(inputFilePath);
+
+            using (reader)
+            {
+                int counter = 0;
+                string line = reader.ReadLine();
+                using (var writer = new StreamWriter(outputFilePath))
+                {
+                    while (line != null)
+                    {
+                        if (counter % 2 == 0)
+                        {
+                            writer.WriteLine(line);
+                        }
+                        counter++;
+                        line = reader.ReadLine();
+                    }
+                    
+
+                }
+
+            }
+        }
+    }
+}
